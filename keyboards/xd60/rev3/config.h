8d4e2a7f5af826c67dc165a9a522d6ce01fa8ddd
Nicolas Ochsner <ochsnern@student.ethz.ch>
2020-07-26 18:55:39 +0200

    Try to enable backlight sleep

--- keyboards/xd60/rev3/config.h
+++ keyboards/xd60/rev3/config.h
@@ -67,6 +67,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
 #define RGBLIGHT_VAL_STEP 17
+#define RGBLIGHT_SLEEP
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
